a55bb3b34ee9f131d627bf2e57f79f7c6f6034ab
Ryan Sise <kerneltux0@gmail.com>
2019-02-26 21:03:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,80 @@
 def reformat_languages(languages)
-  # your code here
+	new_hash = {}
+	languages.each do |category,langs|
+		if category == "oo"
+			langs.each do |name,aspect|
+				aspect.each do |typeKey,typeVal|
+					new_hash[name] = {
+						:type => typeVal,
+						:style => []
+					}
+				end
+			end
+					new_hash[name][:style] << category
+		elsif category == "functional"
+			langs.each do |name,aspect|
+				aspect.each do |typeKey,typeVal|
+					new_hash[name] = {
+						:type => typeVal,
+						:style => []
+					}
+				end
+			end
+		else
+			nil
+		end
+	end
+	new_hash
 end
+
+def lang
+  {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+   
+    }
+  }
+end
+
+def practice
+	new_hash = {}
+	lang.each do |category,langs|
+		langs.each do |name,aspect|
+			aspect.each do |typeKey,typeVal|
+				new_hash[name] = {
+					:type => typeVal,
+					:style => []
+				}
+				new_hash[name][:style] << category
+			end
+		end
+	end
+	puts new_hash
+end
+
+practice
